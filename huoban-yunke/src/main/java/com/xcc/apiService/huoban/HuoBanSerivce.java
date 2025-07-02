9029a3d5a570f81a461e5777ab12983e56e6fb08
staycalm <hello@staycalm.cn>
2025-07-02 18:23:58 +0800

    优化了HuobanService里获取表格列表的方法，查询数据列表的方法写了一些

--- huoban-yunke/src/main/java/com/xcc/apiService/huoban/HuoBanSerivce.java
+++ huoban-yunke/src/main/java/com/xcc/apiService/huoban/HuoBanSerivce.java
@@ -3,6 +3,7 @@ package com.xcc.apiService.huoban;
 import cn.hutool.http.HttpRequest;
 import cn.hutool.http.HttpUtil;
 import cn.hutool.http.Method;
+import cn.hutool.json.JSONArray;
 import cn.hutool.json.JSONObject;
 import cn.hutool.json.JSONUtil;
 import lombok.extern.slf4j.Slf4j;
@@ -15,26 +16,79 @@ import java.util.Arrays;
 @Slf4j
 @Service
 public class HuoBanSerivce {
-    private static final String API_KEY = "EbZB8162L2WGodKxXgYJcPRAImTS1ylCSCVNE8bl";
+
+    private static final String API_KEY = "WYfpk1NWvy8LtPtU2USlbr7tRq9bo7vFjDN7sYEK";
+
     private static final String URL = "https://api.huoban.com/openapi/v1";
 
     //第一次测试
 
 
     public static void main(String[] args) {
-        list();
+        getTableListAndPrint("4000000007807513");
     }
 
-    public static void list() {
-        HttpRequest post = HttpUtil.createPost(URL+"/table/list");
-//        post.setProxy(new Proxy(Proxy.Type.HTTP, new InetSocketAddress("124.220.90.55", 7482)));
-        post.header("Open-Authorization", "Bearer " + API_KEY);
-        post.header("Content-Type", "application/json;charset=utf-8");
-//        post.body("{\"space_id\":\"\",\"type\":\"sync\"}");
-        post.body("{\n    \"space_id\": \"4000000003442925\"\n}");
-        String body = post.execute().body();
-        System.out.println(body);
+    //获取表格列表（所有表格编号和表格名称）并打印在控制台
+    public static String getTableListAndPrint(String spaceId){
+        String url = URL + "/table/list";
+
+        //按照API文档定义请求头
+        HttpRequest post = HttpUtil.createPost(url);
+        post.header("Open-Authorization", API_KEY);
+        post.header("Content-Type", "application/json");
+
+        //按照API文档定义请求体
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.set("space_id", spaceId);
+        post.body(jsonObject.toString());
+
+        //执行post请求，把返回的JSON信息打印在控制台
+        String response = post.execute().body();
+        System.out.println("表格列表JSON形式：" + response);
+        System.out.println();
+
+        //把JSON信息解析之后打印在控制台
+        JSONObject json = JSONUtil.parseObj(response);
+        int code = json.getInt("code");
+        if(code == 0){
+            JSONArray tables = json.getJSONObject("data").getJSONArray("tables");
+            for(Object obj : tables){
+                JSONObject table = (JSONObject) obj;
+                String tableId = table.getStr("table_id");
+                String name = table.getStr("name");
+                System.out.printf("表格编号：%s, 表格名称：%s%n", tableId, name);
+            }
+        }else{
+            System.out.println("调用接口失败，返回：" + response);
+        }
+
+        return response;
+
     }
+
+    //查询数据列表
+    public static String getDataList(){
+        String url = URL + "/table/list";
+
+        HttpRequest post = HttpUtil.createPost(url);
+        post.header("Open-Authorization", API_KEY);
+        post.header("Content-Type", "application/json");
+
+        return null;
+    }
+
+
+//旧版：获取表格列表的方法
+//    public static void list() {
+//        HttpRequest post = HttpUtil.createPost(URL+"/table/list");
+////        post.setProxy(new Proxy(Proxy.Type.HTTP, new InetSocketAddress("124.220.90.55", 7482)));
+//        post.header("Open-Authorization", "Bearer " + API_KEY);
+//        post.header("Content-Type", "application/json;charset=utf-8");
+////        post.body("{\"space_id\":\"\",\"type\":\"sync\"}");
+//        post.body("{\n    \"space_id\": \"4000000007807513\"\n}");
+//        String body = post.execute().body();
+//        System.out.println(body);
+//    }
 
 //    {
 //        "table_id": "2100000017297977",
@@ -43,6 +97,8 @@ public class HuoBanSerivce {
 //            "space_id": "4000000003442925",
 //            "created_on": "2021-07-19 00:08:30"
 //    },
+
+    //更新线索状态的方法
     public Boolean UpdateHuoBan(JSONObject rquest){
         String item_id = rquest.getStr("item_id");
         String if_effective = rquest.getStr("if_effective");
